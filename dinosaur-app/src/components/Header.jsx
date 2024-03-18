91a74285ff716b556809cd1462aa838d0d7d0b25
laura <lg8104@gmail.com>
2024-03-18 08:30:09 -0600

    Created Header component and added call for header in App.jsx

---
+++ dinosaur-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Dinosaur App</h1>
+        </div>
+    )
+}
+
+export default Header
