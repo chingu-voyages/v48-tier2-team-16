5201ebe3e8558a58e9c9d290086c66c42aa9539f
Kaz Smino <kaz.sminoo@gmail.com>
2024-03-19 21:38:51 -0700

    Display news data in DinosaurDetails component

--- dinosaur-app/src/components/DinosaurDetails.jsx
+++ dinosaur-app/src/components/DinosaurDetails.jsx
@@ -15,7 +15,7 @@ const DinosaurDetails = () => {
         const response = await axios.get(
           `https://newsapi.org/v2/everything?q=${dinosaur.name}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`
         );
-        console.log(response.data.articles.slice(0, 2));
+        setNews(response.data.articles.slice(0, 2));
       } catch (error) {
         console.error("Error fetching news:", error);
       }
@@ -37,6 +37,20 @@ const DinosaurDetails = () => {
             <li className="list-group-item" style={{ backgroundColor: '#e4f7e4' }}>When Lived: {dinosaur.whenLived}</li>
             <li className="list-group-item" style={{ backgroundColor: '#eafaea' }}>Found In: {dinosaur.foundIn}</li>
           </ul>
+          {news.length > 0 ? (
+            news.map((article, index) => (
+              <div key={index} style={{ border: '1px solid #000' }}>
+                <h5>{article.title}</h5>
+                <p>{new Date(article.publishedAt).toLocaleString('en-CA', { dateStyle: 'short', hour12: false, timeStyle: 'short' })}</p>
+                <p>Source: {article.source.name}</p>
+                <p>Author: {article.author}</p>
+                <div dangerouslySetInnerHTML={{ __html: article.content }} />
+                <a href={article.url} className="btn btn-link btn-sm">Read more</a>
+              </div>
+            ))
+          ) : (
+            <p>{"Sorry, we couldn't find any news articles about " + dinosaur.name + "."}</p>
+          )}
         </div>
       </div>
     </div>
